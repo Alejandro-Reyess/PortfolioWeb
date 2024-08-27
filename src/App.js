dfec5e61e13db16226da9cb7de530aa0b7fda277
Alejandro Reyes Silva <alejandro.reyes@dinfor.uniceub>
2024-08-27 12:09:12 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -5,18 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Portfólio Web</h1>
+        <h3><a href="https://alejandro-reyess.github.io/">Alejandro Reyes Silva</a></h3>
       </header>
     </div>
   );
